de8a22fe17e9b761621606bd6564557b2ebbcfeb
Your Name <you@example.com>
2024-10-10 18:09:37 +0300

    finish splash screen

---
+++ lib/features/splash/presentation/view/splash.dart
@@ -0,0 +1,37 @@
+import 'package:chat_app1/core/constants/app_assets.dart';
+import 'package:chat_app1/core/theme/app_styles.dart';
+import 'package:chat_app1/features/home/presentation/view/home.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Home(),
+          ));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Image.asset(AppAssets.logo,height: 250,width: 250,),
+        const Text("Welcome to chat App",style: AppStyles.size24Weight600green,)
+      ],),
+    );
+  }
+}
