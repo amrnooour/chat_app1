1ad4357df26746e0cba079d41b8d9d8f0593014c
Your Name <you@example.com>
2024-10-10 18:04:35 +0300

    add asets and styles

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,4 @@
+abstract class AppAssets {
+  static const basePath = "assets/images/";
+  static const logo = "${basePath}logo.png";
+}
---
+++ lib/core/theme/app_styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const size24Weight600green =
+      TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.green);
+}
