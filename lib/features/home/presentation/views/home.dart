058f83579fef3450c85c53b7f6f47ae4ba0eca50
Your Name <you@example.com>
2024-10-26 14:07:47 +0300

    merge auth feature on dev

--- lib/features/home/presentation/views/home.dart
+++ lib/features/home/presentation/views/home.dart
@@ -3,7 +3,6 @@ import 'package:chat_app1/core/shared_widgets/custom_app_bar.dart';
 import 'package:chat_app1/features/home/presentation/view_model/home_cubit.dart';
 import 'package:chat_app1/features/home/presentation/view_model/home_states.dart';
 import 'package:chat_app1/features/home/presentation/views/widgets/custom_bottom_nav.dart';
-import 'package:chat_app1/features/home/presentation/views/widgets/custom_button_more.dart';
 import 'package:chat_app1/features/home/presentation/views/widgets/custom_floating_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/features/home/presentation/views/widgets/profile_body.dart
+++ lib/features/home/presentation/views/widgets/profile_body.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app1/features/home/presentation/views/widgets/custom_button_profile.dart';
 import 'package:chat_app1/features/home/presentation/views/widgets/custom_profile_image.dart';
 import 'package:chat_app1/features/home/presentation/views/widgets/custom_update_profile.dart';
 import 'package:flutter/material.dart';
