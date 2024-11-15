ce234160933dc2ccc54747971fdf8d8ceaa27d5a
Your Name <you@example.com>
2024-11-16 01:07:45 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:chat_app1/features/splash/presentation/view/splash.dart';
+import 'package:chat_app1/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
